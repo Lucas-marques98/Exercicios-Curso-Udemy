bc90207af772330acbd3e4e049a536d7d11839ef
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-07-08 09:18:01 -0300

    Mais sobre Numbers

---
+++ number.js
@@ -0,0 +1,13 @@
+let num1 = 10; //Number
+let num2 = 11.282480249204902; //Number
+
+console.log(num1.toString(2)) // transforma em Binário
+console.log(num2.toFixed(3)) // para usar somente 3 casas decimais
+console.log(Number.isInteger(num1)) // Para saber se o número é inteiro (true Para sim) (False para não)
+console.log(Number.isNaN) // Para saber se a a conta que está tentando fazer é verdadeiro ou falso.
+
+
+// .toFixed para arredondar números e escolher quantas casas decimais usar
+// .toString para transformar um Number em uma string. 
+// Number.isInteger Para saber se é o Número é inteiro.
+// Number.isNan para saber se a conta é válida ou não.
