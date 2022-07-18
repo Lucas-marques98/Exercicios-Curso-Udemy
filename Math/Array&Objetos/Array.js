27ea882cb3b2d883e14dbe0664ca51bf215ffe1a
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-07-18 10:13:26 -0300

    Novos Exercicios

--- Math/Array&Objetos/Array.js
+++ Math/Array&Objetos/Array.js
@@ -7,14 +7,16 @@ console.log(nome[6]) // buscando indixes de uma string
 //forma de arrays indexisados
 //                  0         1         2           3    ...
 const pessoas = ['Lucas', 'Milena', 'Neide', 400040440,]
-//Adicionando elementos no começo de um Array
 
+//Adicionando elementos no começo de um Array
 pessoas.unshift('Lucas Rodrigues')
 
-
+// pessoas.pop(); // remove o elemento do final // eu posso guardar o que foi removido do array em uma Váriavel. const removido = pessoas.pop(); // se usar SHIFT ele remove do começo.
+// para descobrir se é um Array // console.log(pessoas instaceof Array)
 // Exemplos de Adicionar Elementos no fim de um Array
 // pessoas[0] = 'Lukinhas'  trocando valor de um Array
 // pessoas[4] = 'Marques' // Adicionando um elemento sem adicionar dentro da Váriavel.
 // pessoas[pessoas.length] = 'Rodrigues' // Outra forma de adicionar um elemento ou
 // pessoas.push('Lucas') Adiciona elementos no fim de um Array
+// console.log(pessoas.slice (0, -2))// para fazer fatiamento em no Array (da para fazer positivo ou negativo)
 console.log(pessoas) 
