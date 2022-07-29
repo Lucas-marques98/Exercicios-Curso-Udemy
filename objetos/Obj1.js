e89400ab3423b735fcc8e42e2bbd00e9e8786d93
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-07-29 18:03:23 -0300

    objeto ex

--- objetos/Obj1.js
+++ objetos/Obj1.js
@@ -5,14 +5,15 @@ const pessoa1 = {
   endereço: 'Rua Não te interessa'
 }; // objeto literal
 
-// dentro do escopo do Objeto eu posso criar váriaveis e atributos // basta eu dar o nome do atributo, agregar o valor com dois pontos (:) e se eu quiser separar os atributos eu devo usar as virgulas.
+// dentro do escopo do Objeto eu posso criar váriaveis e atributos // basta eu dar o nome do atributo, agregar o valor com dois pontos (:) e se eu quiser separar os atributos 
+// eu devo usar as virgulas.
 
 // para eu conseguir acessar o atributo do Objeto, devo colocar o nome do objeto, depois o ponto (.) e por ultimo o nome do atributo.
 
 console.log(`Eu ${pessoa1.nome} ${pessoa1.sobrenome} tenho ${pessoa1.idade} anos e moro na ${pessoa1.endereço}`)
 
-function criaPessoa (nome, sobrenome, idade){ // nome dos parametros
-  return{
+function criaPessoa(nome, sobrenome, idade) { // nome dos parametros
+  return {
     nome: nome,
     sobrenome: sobrenome, // função que cria Objetos
     idade: idade
@@ -20,9 +21,9 @@ function criaPessoa (nome, sobrenome, idade){ // nome dos parametros
 }
 //criando constantes que guardar uma função de dar nome aos parametros da função.
 const pessoas1 = criaPessoa('Lucas', 'Marques', 24)
-const pessoas2= criaPessoa('Neide', 'Marques', 43)
+const pessoas2 = criaPessoa('Neide', 'Marques', 43)
 const pessoas3 = criaPessoa('Luiza', 'Oliveira', 21)
 const pessoas4 = criaPessoa('Lara', 'moreira', 20)
 const pessoas5 = criaPessoa('Leticia', 'silva', 25)
- 
-// argumento sendo passado para o parametro. 
+
+// argumentos sendo passado para o parametro. ☝ 
