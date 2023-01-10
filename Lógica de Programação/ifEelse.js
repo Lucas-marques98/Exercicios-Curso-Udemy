b8d64d45c5acd5815236c4cdc4e8d7b2654263d8
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2023-01-09 21:43:06 -0300

    Apenas add um ex básico

---
+++ Lógica de Programação/ifEelse.js
@@ -0,0 +1,16 @@
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+  console.log('Sim o número está entre 0 e 5')
+}
+else if (numero >= 6 && numero <= 8) {
+  console.log('Sim o numéro está entre 6 e 8 ')
+}
+else if (numero >= 9 && numero <= 11) {
+
+}
+else {
+  console.log('Não está entre 0 e 5')
+}
+
+//APENAS UM TREINAMENTO 
